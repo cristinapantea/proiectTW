a91d93ceaacd10310292423d6dd550f5cb6eb1dd
cristinapantea <cristina.pantea02@e-uvt.ro>
2022-12-15 16:22:52 +0200

    proiect

---
+++ routes.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var router = express.Router();
+
+
+router.get("/", function(req, res) {
+    // console.log("hello I'm on the start page");
+res.render("index");
+});
+// router.get("/", (req, res) => {
+//     res.render("filme");
+//   });
+module.exports = router;
